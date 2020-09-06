884fec20fa50d225214b5b32fb5ded28cf8656c7
darrrya21 <56168042+darrrya21@users.noreply.github.com>
2020-09-06 11:21:54 +0300

    Add files via upload

---
+++ 6. Объекты и классы. Первая программа.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std; 
+
+class smallobj
+{
+    private: 
+        int somedata;
+    public:
+        void setdata(int d) //метод класса
+            {somedata = d;}
+        void showdata()
+            {cout << "Значение поля равно: " << somedata << endl;}
+        
+}; 
+
+int main()
+{
+    smallobj s1, s2; 
+    s1.setdata(100); 
+    s2.setdata(101); 
+    s1.showdata(); 
+    s2.showdata();
+    return 0;
+}
